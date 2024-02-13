072f56ab32c0233155fb69618bc982aa3e92b7fa
Cayleigh <cayleighl@byui.edu>
2024-02-12 23:26:13 -0700

    added scripts

---
+++ chamber/script/getdate.js
@@ -0,0 +1,8 @@
+const date = new Date();
+
+const yearString= date.toLocaleDateString('en-US', {
+    year: "numeric"
+});
+
+document.querySelector("#copyrightYear").innerHTML = yearString;
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
